06dc1865707423c7b8a873e19fdcf4001a2e22a2
Rafael <dallmann.rafa@gmail.com>
2019-10-01 19:49:14 -0300

    adição cadastro-controller

---
+++ src/main/webapp/js/controller/cadastro-controller.js
@@ -0,0 +1,29 @@
+appEntra21.controller("usuarioController", function($scope, $http) {
+
+$scope.listaUsuario = [];
+$scope.usuario = {};
+
+var urlApi = 'http://localhost:8080/walletControl/rest/';
+
+$scope.salvarUsuario = function() {
+	var metodo = 'POST';
+
+	if ($scope.usuario.id) {
+		metodo = 'PUT';
+	}
+
+	$http({
+		method : metodo,
+		url : urlApi + 'usuarios/',
+		data : $scope.usuario
+	}).then(function(response) {
+		$scope.listaUsuario.push(response.data);
+		// $scope.listarUsuarios();
+	}, function(response) {
+		console.log('error do salvar');
+		console.log(response.data);
+		console.log(response.status);
+	});
+	};
+
+});
