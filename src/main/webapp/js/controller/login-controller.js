2bf92ffabef65cc4356d538809e75d14d6934320
josue <josuekujat@gmail.com>
2019-10-02 21:45:28 -0300

    login melhorado

--- src/main/webapp/js/controller/login-controller.js
+++ src/main/webapp/js/controller/login-controller.js
@@ -1,57 +1,29 @@
-appEntra21.controller("loginController", function ($scope, $http, $window, $location) {
+appEntra21.controller("loginController", function($scope, $http, $window,
+		$location) {
 
-    $scope.usuario = {};
-    var urlApi = 'rest/login/autenticar/'
+	$scope.usuario = {};
+	$scope.mensagem=""
+	var urlApi = 'rest/login/autenticar/'
 
-    $scope.autenticar = function () {
+	$scope.autenticar = function() {
 
+		$http.post(urlApi, $scope.usuario).then(
+				function(response) {
 
+					console.log(response.data);
 
-        $http.post(urlApi, $scope.usuario).then(
-            function (response) {
+					if (response.data.email == $scope.usuario.email
+							&& response.data.senha == $scope.usuario.senha) {
+						$location.path('despesas');
 
-                console.log(response.data);
+					}
 
-                if (response.data.email == $scope.usuario.email &&
-                    response.data.senha == $scope.usuario.senha) {
-                    $location.path('despesas');
+				}, function(response) {
+					console.log(response.status );
+					if(response.status=='500'){
+						$scope.mensagem = "Verificar usuario ou senha"
+					}
+				});
 
-                    console.log("indo para outra tela");
-                }
-
-            },
-            function (response) {
-                console.log("falha " + response);
-            });
-
-
-
-
-
-
-    }
+	}
 });
-
-appEntra21.controller("loginController", function ($scope, $http, $window, $location) {
-
-    $scope.usuario = {};
-    var urlApi = 'rest/login/autenticar/'
-
-    $scope.autenticar = function () {
-
-        $http.post(urlApi, $scope.usuario).then(
-            function (response) {
-
-                console.log(response.data);
-
-                if (response.data.email == $scope.usuario.email &&
-                    response.data.senha == $scope.usuario.senha) {
-                    $location.path('despesas');
-                }
-            },
-            function (response) {
-                console.log("falha " + response);
-            });
-    }
-});
-
